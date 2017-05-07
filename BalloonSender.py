d5d1b68f01705cda46933853bb6dacb1d91af897
Wang Mingwei <cloveriiw@gmail.com>
2017-05-08 00:19:00 +0800

    v0.3.0

--- BalloonSender.py
+++ BalloonSender.py
@@ -1,23 +1,49 @@
 # -*- coding: utf-8 -*-
+# 1. when the window is placed in the center, AC >= 295 boom
+# 2. add the judge Contest Unavailable!
+import re
 import json
 import urllib  
 import urllib2
 from Tkinter import *
 import cookielib
 
-def getStatus():
-    data = conf['cinfo']
-    cid = data['cid']
-    iDisplayStart = data['iDisplayStart']
-    iDisplayLength = data['iDisplayLength']
-    sSearch_3 = data['sSearch_3']
-    
-    cookie = cookielib.CookieJar()
+def tryAccess():
+    cookie = cookielib.MozillaCookieJar()
+    if useCookies:
+        cookie.load('cookies.txt', ignore_discard=True, ignore_expires=True)
+    request = urllib2.Request('http://acm.cqu.edu.cn/contest_info.php?cid='+ conf['cinfo']['cid'])
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
-    req = urllib2.Request('http://acm.cqu.edu.cn/ajax/contest_status_data.php?cid=' + cid + '&randomid=0.8867441343120868&sEcho=5&iColumns=10&sColumns=&iDisplayStart=' + iDisplayStart + '&iDisplayLength=' + iDisplayLength + '&mDataProp_0=0&mDataProp_1=1&mDataProp_2=2&mDataProp_3=3&mDataProp_4=4&mDataProp_5=5&mDataProp_6=6&mDataProp_7=7&mDataProp_8=8&mDataProp_9=9&sSearch=&bRegex=false&sSearch_0=&bRegex_0=false&bSearchable_0=true&sSearch_1=&bRegex_1=false&bSearchable_1=true&sSearch_2=&bRegex_2=false&bSearchable_2=true&sSearch_3=' + sSearch_3 + '&bRegex_3=false&bSearchable_3=true&sSearch_4=&bRegex_4=false&bSearchable_4=true&sSearch_5=&bRegex_5=false&bSearchable_5=true&sSearch_6=&bRegex_6=false&bSearchable_6=true&sSearch_7=&bRegex_7=false&bSearchable_7=true&sSearch_8=&bRegex_8=false&bSearchable_8=true&sSearch_9=&bRegex_9=false&bSearchable_9=true&iSortCol_0=1&sSortDir_0=desc&iSortingCols=1&bSortable_0=false&bSortable_1=false&bSortable_2=false&bSortable_3=false&bSortable_4=false&bSortable_5=false&bSortable_6=false&bSortable_7=false&bSortable_8=false&bSortable_9=false&_=1494052227604')
-    res = opener.open(req)
+    result = opener.open(request).read()
     
-    return json.load(res)
+    pattern = re.compile(r'Contest Unavailable!')
+    ret = re.match(pattern,result)
+    return ret != None
+
+def login():
+    filename = 'cookies.txt'
+    cookie = cookielib.MozillaCookieJar(filename)
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
+    postdata = urllib.urlencode(conf['userinfo'])
+    req = urllib2.Request(url = 'http://acm.cqu.edu.cn/ajax/login.php', data = postdata)
+    res = opener.open(req)
+    cookie.save(ignore_discard=True, ignore_expires=True)
+    
+    return res.read() == '{"code":0,"msg":"Success..."}'
+
+def getStatus():
+    cookie = cookielib.MozillaCookieJar()
+    if useCookies:
+        cookie.load('cookies.txt', ignore_discard=True, ignore_expires=True)
+    data = urllib.urlencode(conf['cinfo'])
+    request = urllib2.Request('http://acm.cqu.edu.cn/ajax/contest_status_data.php?' + data + 'randomid=0.8867441343120868&sEcho=5&iColumns=10&sColumns=&mDataProp_0=0&mDataProp_1=1&mDataProp_2=2&mDataProp_3=3&mDataProp_4=4&mDataProp_5=5&mDataProp_6=6&mDataProp_7=7&mDataProp_8=8&mDataProp_9=9&sSearch=&bRegex=false&sSearch_0=&bRegex_0=false&bSearchable_0=true&sSearch_1=&bRegex_1=false&bSearchable_1=true&sSearch_2=&bRegex_2=false&bSearchable_2=true&bRegex_3=false&bSearchable_3=true&sSearch_4=&bRegex_4=false&bSearchable_4=true&sSearch_5=&bRegex_5=false&bSearchable_5=true&sSearch_6=&bRegex_6=false&bSearchable_6=true&sSearch_7=&bRegex_7=false&bSearchable_7=true&sSearch_8=&bRegex_8=false&bSearchable_8=true&sSearch_9=&bRegex_9=false&bSearchable_9=true&iSortCol_0=1&sSortDir_0=desc&iSortingCols=1&bSortable_0=false&bSortable_1=false&bSortable_2=false&bSortable_3=false&bSortable_4=false&bSortable_5=false&bSortable_6=false&bSortable_7=false&bSortable_8=false&bSortable_9=false&_=1494052227604')
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
+    response = opener.open(request)
+    result = response.read()
+    if isNotBlank(result):
+        return json.loads(result)
+    else:
+        return {}
     
 def getNewAC():
     global lastRecord, total
@@ -67,29 +93,43 @@ def removeItems():
         del(todoLst[i])
     del rmLst[:]
 
+def isNotBlank (myString):
+    return bool(myString and myString.strip())
+
 def main():
-    global lastRecord, total, status, todoLst, rmLst, records, conf
+    global lastRecord, total, status, todoLst, rmLst, records, conf, useCookies
     lastRecord = -1
     total = 0
     todoLst = []
     rmLst = []
     records = set()
     flag = True
+    useCookies = False
     with open('conf.json', 'r') as f:    
         conf = json.load(f)
     
+    if 'userinfo' in conf:
+        uinfo = conf['userinfo']
+        if 'username' in uinfo and 'password' in uinfo:
+            if isNotBlank(uinfo['username']) and isNotBlank(uinfo['password']):
+                if login():
+                    useCookies = True
+    
+    if useCookies and not tryAccess():
+        print('Contest Unavailable! Check your config or permisson to the contest')
+        return
+    
     while(flag):
+        root = Tk()
         removeItems()
         status = getStatus()
-        #print(existNewAC())
-        if existNewAC():
+        #print(status)
+        if status and existNewAC():
             getNewAC()
+        else:
+            label(root, LEFT, 'ERROR! Please check your config.')
         
-        root = Tk()
-        root.withdraw()  
-        screen_width = root.winfo_screenwidth()
-        screen_height = root.winfo_screenheight() - 100    #under windows, taskbar may lie under the screen
-        root.resizable(False,False)
+        
         root.title('Balloon Sender')
         root.attributes("-topmost", 1)
 
@@ -103,12 +143,7 @@ def main():
                     label(keyF, LEFT, txt)
                 button(keyF, LEFT, 'Done', command = lambda i=i: addToRmLst(i))  
                 # need to notify when a button is clicked twice
- 
-        root.update_idletasks()
-        root.deiconify()    # calculate window size
-        root.withdraw()     # hide window
-        root.geometry('%sx%s+%s+%s' % (root.winfo_width() + 10, root.winfo_height() + 10, (screen_width - root.winfo_width())/2, (screen_height - root.winfo_height())/2) )    # center window
-        root.deiconify()
+
         root.mainloop()
 
 if __name__ == '__main__':
